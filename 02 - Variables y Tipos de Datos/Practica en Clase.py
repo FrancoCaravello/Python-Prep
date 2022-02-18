f5885cc590a79410688b77e559f3a3d89e23082b
FrancoCaravello <francoivan96@gmail.com>
2022-02-18 17:53:42 -0300

    Tercer commit

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,2 +1,42 @@
 mi_variable = 'Franco'
 print(mi_variable)
+
+
+for n in range(1,10):
+    print(n)
+
+print('')
+
+m = 1
+while (m < 10):
+    print(m)
+    m += 1
+
+print('')
+
+
+estaciones = ('verano', 'otoño', 'invierno', 'primavera')
+for estacion in estaciones:
+    print(estacion)
+
+print('')
+
+for indice,epoca in enumerate(estaciones):
+    print('Estamos en ',epoca,' que es representada por el numero ',(indice+1))
+
+
+print('')
+
+for n in range(1,10):
+    print(n)
+    if (n == 5):
+        break
+
+print('Se termina el bucle cuando llega al break')
+
+for n in range(1,10):
+    if (n == 5):
+        continue
+    print(n)
+
+print('Ignoro la vuelva del bucle')
